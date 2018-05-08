12bf6feba6fb01d5316b276e8539a5f5ac0bca9d
Yifan You <harry47341@yahoo.com>
2018-05-08 16:30:05 -0700

    Add files via upload

---
+++ get_rand_rows.m
@@ -0,0 +1,5 @@
+function [rand]=get_rand_rows(X,num)
+  m=size(X)(1);
+  indices=randperm(m,num);
+  rand=X(indices,:);
+endfunction
---
+++ learningCurve_average.m
@@ -0,0 +1,28 @@
+function [error_train,error_val]=learningCurve_average(X, y, Xval, yval, lambda)
+  num_repeat=50;
+  m=size(X)(1);
+  error_train = zeros(m, 1);
+  error_val   = zeros(m, 1);
+
+  for i=1:m
+    for j=1:num_repeat
+      %choose i random samples from X and y
+     indices=randperm(m,i);
+      X_sample=X(indices,:);
+      y_sample=y(indices,:);
+      
+      %choose i random samples from Xval and yval
+      indices=randperm(m,i);
+      Xval_sample=Xval(indices,:);
+      yval_sample=yval(indices,:);
+      
+      %get error_train and error_val
+      theta=trainLinearReg(X_sample,y_sample,lambda);
+      error_train(i)+=linearRegCostFunction(X_sample,y_sample,theta,0);
+      error_val(i)+=linearRegCostFunction(Xval_sample,yval_sample,theta,0);
+      
+    endfor
+  endfor
+  error_train=error_train./num_repeat;
+  error_val=error_val./num_repeat;
+endfunction
